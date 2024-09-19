db7fab3c9f597f19e465f180ae0c6e716697e45c
sibzairdrop <sinarogtrade@gmail.com>
2024-09-19 15:22:29 +0330

    Update script.js

--- script.js
+++ script.js
@@ -161,6 +161,27 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: '4bdc17da-2601-449b-948e-f8c7bd376553',
             timing: 20000, // 30 seconds
             attempts: 30,
+        },
+        18: {
+            name: 'Infected Frontier',
+            appToken: 'eb518c4b-e448-4065-9d33-06f3039f0fcb',
+            promoId: 'eb518c4b-e448-4065-9d33-06f3039f0fcb',
+            timing: 20000, // 30 seconds
+            attempts: 30,
+        },
+        19: {
+            name: 'Among Water',
+            appToken: 'daab8f83-8ea2-4ad0-8dd5-d33363129640',
+            promoId: 'daab8f83-8ea2-4ad0-8dd5-d33363129640',
+            timing: 20000, // 30 seconds
+            attempts: 30,
+        },
+        20: {
+            name: 'Factory World',
+            appToken: 'd02fc404-8985-4305-87d8-32bd4e66bb16',
+            promoId: 'd02fc404-8985-4305-87d8-32bd4e66bb16',
+            timing: 20000, // 30 seconds
+            attempts: 30,
         }
     };
 
